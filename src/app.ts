43cba0f8aa75781c596351009e0d57a0b78d7a2c
001Rakib <rakibrakibrakib354@gmail.com>
2024-06-12 14:38:36 +0600

    created user model and interface

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import router from "./app/routes";
+const app: Application = express();
+
+//parser
+app.use(express.json());
+app.use(cors());
+
+//application routes
+app.use("/api", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
