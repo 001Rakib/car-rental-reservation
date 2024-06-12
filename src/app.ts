e798f8a01f346749b60ea5ac874dd0061d73ed52
001Rakib <rakibrakibrakib354@gmail.com>
2024-06-12 15:55:39 +0600

    global, cast, zod, duplicate error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
+import notFound from "./app/middleware/notFound";
+import globalErrorHandler from "./app/middleware/globalErrorHandlers";
 const app: Application = express();
 
 //parser
@@ -13,5 +15,9 @@ app.use("/api", router);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+//global error handler
+app.use(globalErrorHandler);
+//not found
+app.use(notFound);
 
 export default app;
--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export default {
   port: process.env.PORT,
   database_url: process.env.DB_URL,
+  node_env: process.env.NODE_ENV,
 };
